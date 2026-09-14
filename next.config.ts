50efcfed3d3eca51c4d07049208297218e19a7f4
Harsh Vardhan <harshvardhan.p27@gmail.com>
2026-09-15 02:21:15 +0530

    chore: relax CSP/COOP headers for Google sign-in

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://www.googletagmanager.com https://www.google-analytics.com;
+script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://apis.google.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src 'self' blob: data: https:;
   font-src 'self' data: https://fonts.gstatic.com;
   connect-src 'self' https: http: ws: wss:;
-  frame-src 'self' https://accounts.google.com https://*.google.com https://*.firebaseapp.com;
+  frame-src 'self' https://accounts.google.com https://*.google.com https://redefine-26.firebaseapp.com;
   media-src 'self';
   object-src 'none';
   base-uri 'self';
@@ -45,6 +45,10 @@ const nextConfig: NextConfig = {
           {
             key: "Permissions-Policy",
             value: "camera=(), microphone=(), geolocation=()",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
           },
         ],
       },
